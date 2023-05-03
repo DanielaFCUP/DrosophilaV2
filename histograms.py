ed163c1ceb64d9024dbc6fcd17de5ef886a9a93f
Daniela Maria da Silva Tavares <danielatavares97@hotmail.com>
2023-05-03 14:07:01 +0100

    Script to plot histograms for each specie

---
+++ histograms.py
@@ -0,0 +1,11 @@
+import cv2
+
+# Read an image of a melanogaster
+# Histogram of the image binarizes
+# Histogram of the RGB image
+
+# Read an image of a nova
+
+# Read an image of an americana
+
+
